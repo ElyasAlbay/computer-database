93bc6f9903c1862c1fc01d912b1efe7ed94fd7ea
Elyas ALBAY <ealbay@excilys.com>
2017-05-11 12:13:26 +0200

    [Tests] Tests de non-regression.

--- src/com/excilys/cdb/ui/Main.java
+++ src/com/excilys/cdb/ui/Main.java
@@ -1,17 +1,23 @@
 package com.excilys.cdb.ui;
 
+import java.util.List;
+
+import com.excilys.cdb.model.*;
 import com.excilys.cdb.persistence.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		List<Computer> computersList = null;
 		
 			//CompanyDaoImpl companyDao = new CompanyDaoImpl();
 			ComputerDaoImpl computerDao = new ComputerDaoImpl();
 			
-			//companyDao.listRequest();
-			
+			computersList = computerDao.listRequest();
+			for(Computer computer : computersList) {
+				System.out.println(computer.getName());
+			}
 			//computerDao.listRequest();
-			computerDao.showDetails(5);
+			//computerDao.showDetails(5);
 	}
 }
