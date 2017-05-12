cc5118d4eebc0e3af59d2140b1ca838cc2625893
ElyasAlbay <ealbay@excilys.com>
2017-05-12 17:57:30 +0200

    [Model] Ajout des fonctions hashCOde() et equals().

--- src/com/excilys/cdb/model/Company.java
+++ src/com/excilys/cdb/model/Company.java
@@ -2,7 +2,7 @@ package com.excilys.cdb.model;
 
 /**
  * Model for a company.
- * @author excilys
+ * @author Elyas Albay
  *
  */
 public class Company {
@@ -21,16 +21,65 @@ public class Company {
 	}
 	
 	
+	/**
+	 * Returns Company instance hash code.
+	 * @return result Generated Hash Code.
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	
+	/**
+	 * Returns true if instance is equal to the paramater. False else.
+	 * @param obj Object to compare.
+	 * @return boolean
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Company other = (Company) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}	
+	
+	/**
+	 * Converts company to a string detailing its fields.
+	 */
+	@Override
+	public String toString() {
+		String delimit = " | ";
+		String string = this.getId() + delimit + this.getName();
+		
+		return string;
+	}
+
+
 	/* Getters and setters */
 	public int getId () {
 		return this.id;
 	}
-	
+
 	public void setId (int id) {
 		if(id >= 0) {
 			this.id = id;
 		} else {
-			System.out.println("Error in Company : Incorrect Id");
+			System.err.println("Error in Company : Incorrect Id");
 		}
 	}
 	
--- src/com/excilys/cdb/model/Computer.java
+++ src/com/excilys/cdb/model/Computer.java
@@ -14,6 +14,7 @@ public class Computer {
 	private LocalDate discontinued;
 	private int companyId;
 	
+	
 	/**
 	 * Constructor using identifier and name of the computer.
 	 * @param pId Unique identifier for the computer.
@@ -27,8 +28,61 @@ public class Computer {
 	
 	
 	/**
+	 * Returns Computer instance hash code.
+	 * @return result Generated Hash Code.
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + companyId;
+		result = prime * result + ((discontinued == null) ? 0 : discontinued.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((introduced == null) ? 0 : introduced.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/**
+	 * Returns true if instance is equal to the paramater. False else.
+	 * @param obj Object to compare.
+	 * @return boolean
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Computer other = (Computer) obj;
+		if (companyId != other.companyId)
+			return false;
+		if (discontinued == null) {
+			if (other.discontinued != null)
+				return false;
+		} else if (!discontinued.equals(other.discontinued))
+			return false;
+		if (id != other.id)
+			return false;
+		if (introduced == null) {
+			if (other.introduced != null)
+				return false;
+		} else if (!introduced.equals(other.introduced))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	/**
 	 * Converts computer into a string detailing its fields.
 	 */
+	@Override
 	public String toString() {
 		String delimit = " | ";
 		String string = this.getId() + delimit + this.getName() + delimit + this.getIntroduced() + delimit + this.getDiscontinued() +delimit + this.getCompanyId();
