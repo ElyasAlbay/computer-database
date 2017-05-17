c5af691b7166ca4aaadefe9cfcdc060a570ef4b2
ElyasAlbay <ealbay@excilys.com>
2017-05-17 14:50:08 +0200

    [Test] Ajout du test du Company Dao.

--- src/main/java/com/excilys/cdb/exceptions/InvalidCommandException.java
+++ src/main/java/com/excilys/cdb/exceptions/InvalidCommandException.java
@@ -1,37 +1,50 @@
 package com.excilys.cdb.exceptions;
 
+/**
+ * New exception for invalid user input.
+ * 
+ * @author Elyas Albay
+ *
+ */
 public class InvalidCommandException extends Exception {
 	private static final long serialVersionUID = 27008857691171309L;
 
 	/**
 	 * Class constructor.
 	 */
-	public InvalidCommandException() {}
-	
+	public InvalidCommandException() {
+	}
+
 	/**
 	 * Class constructor.
-	 * @param message Exception detail.
+	 * 
+	 * @param message
+	 *            Exception detail.
 	 */
 	public InvalidCommandException(String message) {
 		super(message);
 	}
-	
+
 	/**
 	 * Class constructor.
-	 * @param cause Cause of the exception.
+	 * 
+	 * @param cause
+	 *            Cause of the exception.
 	 */
 	public InvalidCommandException(Throwable cause) {
 		super(cause);
 	}
-	
+
 	/**
 	 * Class constructor.
-	 * @param message Exception detail.
-	 * @param cause Cause of the exception.
+	 * 
+	 * @param message
+	 *            Exception detail.
+	 * @param cause
+	 *            Cause of the exception.
 	 */
 	public InvalidCommandException(String message, Throwable cause) {
 		super(message, cause);
 	}
-	
-	
+
 }
