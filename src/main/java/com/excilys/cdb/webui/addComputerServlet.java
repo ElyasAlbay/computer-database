5809fde4c7dc8f2f2e6da37c9f801476d8cc81e5
ElyasAlbay <ealbay@excilys.com>
2017-05-17 18:07:51 +0200

    [WebUi] Initialisation du serveur TomCat.

---
+++ src/main/java/com/excilys/cdb/webui/addComputerServlet.java
@@ -0,0 +1,24 @@
+package com.excilys.cdb.webui;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class addComputerServlet extends HttpServlet {	
+
+	private static final long serialVersionUID = 1921946279906681045L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		super.doPost(req, resp);
+	}
+		
+	
+	
+
+
+}
